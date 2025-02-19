e07401886504c75c2eb0518c66fc475296f593ee
VictorWhile <victor.rothmann@outlook.com>
2025-02-19 11:04:54 -0300

    ajuste de process novamente

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // const { NODE_ENV } = process.env;
 // const URL = "http://localhost:3001/api";
-const URL = "https://projeto-clone-spotify-k9m0.onrender.com/api";
+const URL = "https://projeto-clone-spotify-k9m0.onrender.com";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
